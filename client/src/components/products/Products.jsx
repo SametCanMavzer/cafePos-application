0f849925c790d6411ed5c0161a9f143befd785ad
Samet Mavzer <samet.mavzer@intern.priviasecurity.com>
2023-12-19 17:44:35 +0300

    Prodcuts GET action added

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,78 +1,41 @@
+import { useEffect, useState } from "react"
 
 
 const Products = () => {
+
+    const [prodcuts, setProdcuts] = useState([]);
+
+
+    useEffect(() => {
+
+        const getProducts = async () => {
+            try {
+                const res = await fetch("http://localhost:5000/api/products/get-all");
+                const data = await res.json();
+                setProdcuts(data)
+
+            } catch (error) {
+                console.log(error)
+            }
+            getProducts()
+        }
+    }, [])
+
+
     return (
         <div className="produts-wrapper grid grid-cols-card gap-4 ">
-            <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-                <div className="produts-img">
-                    <img src="logo192.png" alt="" className="h-28 object-contain w-full border-b" />
-                </div>
-                <div className="produt-info flex flex-col p-3">
-                    <span className="font-bold">Elma</span>
-                    <span>12₺</span>
-                </div>
+            {prodcuts.map((item) => (
+                <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
+                    <div className="produts-img">
+                        <img src={item.img} alt="" className="h-28 object-contain w-full border-b" />
+                    </div>
+                    <div className="produt-info flex flex-col p-3">
+                        <span className="font-bold">{item.title}</span>
+                        <span>{item.price}</span>
+                    </div>
 
-            </div>
-            <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-                <div className="produts-img">
-                    <img src="logo192.png" alt="" className="h-28 object-contain w-full border-b" />
                 </div>
-                <div className="produt-info flex flex-col p-3">
-                    <span className="font-bold">Elma</span>
-                    <span>12₺</span>
-                </div>
-
-            </div>
-            <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-                <div className="produts-img">
-                    <img src="logo192.png" alt="" className="h-28 object-contain w-full border-b" />
-                </div>
-                <div className="produt-info flex flex-col p-3">
-                    <span className="font-bold">Elma</span>
-                    <span>12₺</span>
-                </div>
-
-            </div>
-            <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-                <div className="produts-img">
-                    <img src="logo192.png" alt="" className="h-28 object-contain w-full border-b" />
-                </div>
-                <div className="produt-info flex flex-col p-3">
-                    <span className="font-bold">Elma</span>
-                    <span>12₺</span>
-                </div>
-
-            </div>
-            <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-                <div className="produts-img">
-                    <img src="logo192.png" alt="" className="h-28 object-contain w-full border-b" />
-                </div>
-                <div className="produt-info flex flex-col p-3">
-                    <span className="font-bold">Elma</span>
-                    <span>12₺</span>
-                </div>
-
-            </div>
-            <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-                <div className="produts-img">
-                    <img src="logo192.png" alt="" className="h-28 object-contain w-full border-b" />
-                </div>
-                <div className="produt-info flex flex-col p-3">
-                    <span className="font-bold">Elma</span>
-                    <span>12₺</span>
-                </div>
-
-            </div>
-            <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-                <div className="produts-img">
-                    <img src="logo192.png" alt="" className="h-28 object-contain w-full border-b" />
-                </div>
-                <div className="produt-info flex flex-col p-3">
-                    <span className="font-bold">Elma</span>
-                    <span>12₺</span>
-                </div>
-
-            </div>
+            ))}
         </div>
     )
 }

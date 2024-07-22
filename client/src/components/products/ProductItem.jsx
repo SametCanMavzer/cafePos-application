b0711eee28e4466b5b6361aa825731c169aaf8fd
Samet Mavzer <samet.mavzer@intern.priviasecurity.com>
2024-07-22 20:09:47 +0300

    redux-toolkit integration is complete and CartSlice is done

--- client/src/components/products/ProductItem.jsx
+++ client/src/components/products/ProductItem.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import { addProduct } from "../../redux/cartSlice";
+import { useDispatch } from "react-redux";
+import { message } from "antd";
 
 const ProductItem = ({ item }) => {
+    const dispatch = useDispatch();
+
+    const handleClick = () => {
+        dispatch(addProduct({ ...item, quantity: 1 }));
+        message.success("Ürün Sepete Eklendi.")
+    };
+
     return (
-        <div className="produts-item border hover:shadow-lg cursor-pointer transition-all select-none">
-            <div className="produts-img">
-                <img src={item.img} alt="" className="h-28 object-contain w-full border-b" />
+        <div
+            className="product-item border hover:shadow-lg cursor-pointer transition-all select-none"
+            onClick={handleClick}
+        >
+            <div className="product-img">
+                <img
+                    src={item.img}
+                    alt=""
+                    className="h-28 object-cover w-full border-b"
+                />
             </div>
-            <div className="produt-info flex flex-col p-3">
+            <div className="product-info flex flex-col p-3">
                 <span className="font-bold">{item.title}</span>
-                <span>{item.price}</span>
+                <span>{item.price}₺</span>
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default ProductItem
+export default ProductItem;
